84bcf37dfdc4ad83f664e30dbb30f1bc6d223a6f
vivianjiang <yjiang@medallionananlytics.com>
2014-05-27 17:14:22 -0400

    import java

---
+++ java/Practice/src/practice/MaxPoints.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practice;
+
+
+/**
+ *
+ * @author admin
+ */
+public class MaxPoints {
+    public int maxPoints(Point[] points){
+        int max = 1;
+        return max;
+    }
+}
