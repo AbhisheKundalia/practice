8960bf7d1ebacc17f6e9ea8cfa095d6abf3c4ece
vivianj <jiangyy12@gmail.com>
2014-07-24 17:17:03 -0400

    update -07/24

--- java/Practice/src/leetcode/maxSubarray.java
+++ java/Practice/src/leetcode/maxSubarray.java
@@ -9,50 +9,140 @@ package leetcode;
  * @author admin
  */
 public class maxSubarray {
+
     public int maxSubArray(int[] A) {
         int maxSum = 0;
         int sum = 0;
         int i = 0;
-        
-        while(i < A.length && A[i] <= 0){
+
+        while (i < A.length && A[i] <= 0) {
             i++;
         }
-        
-        while(i < A.length){
-            while( i < A.length && A[i] >= 0){
-                sum +=A[i];
+
+        while (i < A.length) {
+            while (i < A.length && A[i] >= 0) {
+                sum += A[i];
                 i++;
             }
-            
-            maxSum = maxSum > sum? maxSum : sum;
-            
-            while( i < A.length && A[i] <= 0){
-                sum +=A[i];
+
+            maxSum = maxSum > sum ? maxSum : sum;
+
+            while (i < A.length && A[i] <= 0) {
+                sum += A[i];
                 i++;
             }
-            
-            if(sum <= 0){
+
+            if (sum <= 0) {
                 sum = 0;
             }
         }
-     
+
         return maxSum;
     }
-    
+
     public int maxSubArray1(int[] A) {
         int maxSum = Integer.MIN_VALUE;
         int sum = 0;
-        
-        for(int i = 0; i < A.length; i++){
-           sum += A[i];
-           
-           maxSum = maxSum > sum? maxSum : sum;
-           
-           if( sum < 0 ){
-               sum = 0;
-           }
+
+        for (int i = 0; i < A.length; i++) {
+            sum += A[i];
+
+            maxSum = maxSum > sum ? maxSum : sum;
+
+            if (sum < 0) {
+                sum = 0;
+            }
         }
-     
+
+        return maxSum;
+    }
+
+    //A[0..j] 的最大和为A[0..j-1] 或者A[i..j]
+    //Here we bottom-up instead of recursion then get the following
+    public int maxSubArray2(int[] A) {
+        if (A.length == 0) {
+            return 0;
+        }
+        int maxSum = A[0];
+        int sum = A[0];
+
+        for (int i = 1; i < A.length; i++) {
+            sum = A[i] > sum + A[i] ? A[i] : sum + A[i];
+
+            maxSum = Math.max(maxSum, sum);
+        }
+
+        return maxSum;
+    }
+
+    public int maxSubArray3(int[] A) {
+        if (A.length == 0) {
+            return 0;
+        }
+
+        return maxSubArray4(A, A.length);
+    }
+
+    public int maxSubArray4(int[] A, int n) {
+        int sum = A[n - 1];
+        int maxSum = sum;
+
+        for (int j = n; j > 0; j--) {
+            sum += A[j - 1];
+            maxSum = Math.max(sum, maxSum);
+        }
+
+        sum = maxSubArray4(A, n - 1);
+
+        return Math.max(maxSum, sum);
+    }
+
+    //divide-conquer
+    public int maxSubArray5(int[] A) {
+        if (A.length == 0) return 0;
+        
+        return maxSubarray(A, 0, A.length - 1);
+    }
+
+    public int maxSubarray(int[] A, int start, int end) {
+        int mid = (start + end) / 2;
+        int leftMax = Integer.MIN_VALUE;
+        int rightMax = Integer.MIN_VALUE;
+        int crossMax = Integer.MIN_VALUE;
+        int maxSum = 0;
+        
+        if(start == end) return A[start];
+        
+        if (mid >= start) {
+            leftMax = maxSubarray(A, start, mid);
+        }
+
+        if (mid < end) {
+            rightMax = maxSubarray(A, mid + 1, end);
+        }
+
+
+        if (mid >= start && mid < end) {
+            int sum = A[mid];
+            crossMax = A[mid];
+            
+            for (int j = mid - 1; j >= start; j--) {
+                sum += A[j];
+                crossMax = Math.max(sum, crossMax);
+            }
+
+            crossMax+= A[mid+1];
+            sum = crossMax;
+            
+            for (int i = mid + 2; i <= end; i++) {
+                sum += A[i];
+                crossMax = Math.max(sum, crossMax);
+            }
+        }
+        
+        maxSum = Math.max(leftMax,rightMax);
+        maxSum = Math.max(maxSum, crossMax);
+        
         return maxSum;
     }
 }
--- java/Practice/src/leetcode/strStr.java
+++ java/Practice/src/leetcode/strStr.java
@@ -9,22 +9,80 @@ package leetcode;
  * @author admin
  */
 public class strStr {
-     public String strStr(String haystack, String needle) {
-         int index1 = 0;
-         int index2 = 0;
-         
-         while(index1 < haystack.length() && index2 < needle.length()){
-             if(haystack.charAt(index1) == needle.charAt(index2)){
-                 index1++;
-                 index2++;
-             }else{
-                 index1 = index1 -  index2 + 1;
-                 index2 = 0;
-             }
-          }
-         
-         if(index2 == needle.length()) return haystack.substring(index1 - index2);
-         
-         return null;    
-     }
+
+    public String strStr(String haystack, String needle) {
+        int index1 = 0;
+        int index2 = 0;
+
+        while (index1 < haystack.length() && index2 < needle.length()) {
+            if (haystack.charAt(index1) == needle.charAt(index2)) {
+                index1++;
+                index2++;
+            } else {
+                index1 = index1 - index2 + 1;
+                index2 = 0;
+            }
+        }
+
+        if (index2 == needle.length()) {
+            return haystack.substring(index1 - index2);
+        }
+
+        return null;
+    }
+
+    //KMP
+    public String strStr2(String haystack, String needle) {
+        
+        if(needle.length() == 0) return haystack;
+        
+        int[] F = new int[needle.length() + 1];
+        F = buildFailureF(needle);
+
+        int j = 0;
+        int i = 0;
+        
+        while (i < haystack.length()) {
+            if (haystack.charAt(i) == needle.charAt(j)) {
+                i++;
+                j++;
+                
+                if (j == needle.length()) {
+                    return haystack.substring(i - j + 1);
+                 }
+            }else if (j == 0) {
+                i++;
+            }else j = F[j];
+        }
+
+
+        return null;
+    }
+
+    public int[] buildFailureF(String needle) {
+        int[] F = new int[needle.length() + 1];
+        F[0] = 0;
+        F[1] = 0;
+
+        for (int i = 2; i <= needle.length(); i++) {
+
+            int t = F[i - 1];
+
+            while (true) {
+                if (needle.charAt(i - 1) == needle.charAt(t)) {
+                    F[i] = t + 1;
+                    break;
+                }
+
+                if (t == 0) {
+                    F[i] = 0;
+                    break;
+                }
+
+                t = F[t];
+            }
+        }
+
+        return F;
+    }
 }
